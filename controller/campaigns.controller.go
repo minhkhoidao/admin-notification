af99435b331759fa9b7793df2b4b9407bd0e42ab
khoidaoo <khoi.dao2812@gmail.com>
2024-08-25 13:58:20 +0700

    feat: add campaign service

--- controller/campaigns.controller.go
+++ controller/campaigns.controller.go
@@ -3,6 +3,7 @@ package controller
 import (
 	"admin-backend/models"
 	"admin-backend/services"
+	"log"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,11 +13,30 @@ type CampaignsController struct {
 	CampaignsService services.CampaignsService
 }
 
-func (ct *CampaignsController) CreateNewCampaign(c *gin.Context) {
+func (ct *CampaignsController) CreateCampaignController(c *gin.Context) {
+	log.Println("test")
+
 	var request models.CampaignsRequest
 
 	err := c.ShouldBindJSON(&request)
 	if err != nil {
 		c.JSON(http.StatusNotAcceptable, models.ErrorResponse{Message: err.Error()})
+		return
 	}
+	campaign := models.Campaigns{
+		Template:       request.Template,
+		Type:           request.Type,
+		Event:          request.Event,
+		Content:        request.Content,
+		SaveTemplate:   request.SaveTemplate,
+		NotificationID: request.NotificationID,
+	}
+
+	err = ct.CampaignsService.CreateNewCampaign(c, &campaign)
+	if err != nil {
+		c.JSON(http.StatusInternalServerError, models.ErrorResponse{Message: err.Error()})
+		return
+	}
+
+	c.JSON(http.StatusOK, campaign)
 }
